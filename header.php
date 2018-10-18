c2a51e0aaa1bf6ba5356a5104e3695adb5fbc86a
aksah3727 <35270001+aksah3727@users.noreply.github.com>
2018-10-17 22:20:38 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>RGI LMS || Home</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href='//fonts.googleapis.com/css?family=Quattrocento+Sans' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--slider-->
+<link href="css/slider.css" rel="stylesheet" type="text/css" media="all"/>
+<script type="text/javascript" src="js/jquery-1.9.0.min.js"></script>
+<script type="text/javascript" src="js/jquery.nivo.slider.js"></script>
+<script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+    });
+    </script>
+</head>
+<body>
+<div class="btm_border">
+<div class="h_bg">
+<div class="wrap">
+	<div class="header">
+					<h1>Radharaman Engineering Collage ,Bhopal</h1>
+		
+		<div class="social-icons">
+			<ul>
+			     <li><a class="facebook" href="#" target="_blank"> </a></li>
+			     <li><a class="twitter" href="#" target="_blank"></a></li>
+			     <li><a class="googleplus" href="#" target="_blank"></a></li>
+			     <li><a class="pinterest" href="#" target="_blank"></a></li>
+			     <li><a class="dribbble" href="#" target="_blank"></a></li>
+			     <li><a class="vimeo" href="#" target="_blank"></a></li>
+		   </ul>
+		</div>	
+		<div class="clear"></div>
+	</div>
+	<div class='h_btm'>
+		<div class='cssmenu'>
+			<ul>
+			    <li class='active'><a href='index.html'><span>Home</span></a></li>
+			    
+                <li><a href='about.html'><span>Student Regstration</span></a></li>
+                <li class='has-sub'><a href='./student/slogin.php'><span>Student Login</span></a></li>
+                <li><a href='about.html'><span>About</span></a></li>
+            
+			    <li><a href='staff.html'><span>Staff</span></a></li>
+			    
+			    <li class='last'><a href='contact.html'><span>Contact</span></a></li>
+			 	<div class="clear"></div>
+			 </ul>
+	</div>
+	
+	<div class="clear"></div>
+</div>
+</div>
+</div>
+</div>
---
+++ index.php
@@ -0,0 +1,153 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>RGI LMS || Home</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href='//fonts.googleapis.com/css?family=Quattrocento+Sans' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--slider-->
+<link href="css/slider.css" rel="stylesheet" type="text/css" media="all"/>
+<script type="text/javascript" src="js/jquery-1.9.0.min.js"></script>
+<script type="text/javascript" src="js/jquery.nivo.slider.js"></script>
+<script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+    });
+    </script>
+</head>
+<body>
+<div class="btm_border">
+<div class="h_bg">
+<div class="wrap">
+	<div class="header">
+					<h1><a href="index.html"><img src="images/logo2.jpg" alt=""></a></h1>
+	
+	</div>
+	<div class='h_btm'>
+		<div class='cssmenu'>
+			<ul>
+			    <li class='active'><a href='index.php'><span>Home</span></a></li>
+			    
+                <li><a href='./student/regstration.php'><span>Student Regstration</span></a></li>
+                <li class='has-sub'><a href='./student/slogin.php'><span>Student Login</span></a></li>
+                <li><a href='about.html'><span>About</span></a></li>
+            
+			    <li><a href='staff.html'><span>Staff</span></a></li>
+			    
+			    <li class='last'><a href='contact.html'><span>Contact</span></a></li>
+                   <li><a href='./librarian/login.php'><span>Faculty Login</span></a></li>
+			 	<div class="clear"></div>
+			 </ul>
+	</div>
+	
+	<div class="clear"></div>
+</div>
+</div>
+</div>
+</div>
+
+<!------ Slider_bg ------------>
+<div class="slider_bg">
+<div class="wrap">
+	<!------ Slider------------>
+		  <div class="slider">
+	      	<div class="slider-wrapper theme-default">
+	            <div id="slider" class="nivoSlider">
+	                <img src="images/banner1.jpg" data-thumb="images/1.jpg" alt="" />
+	                <img src="images/font.jpg" data-thumb="images/2.jpg" alt="" />
+	                <img src="images/font1.jpg" data-thumb="images/3.jpg" alt="" />
+	            </div>
+	        </div>
+	      </div>
+		<!------End Slider ------------>
+		<div class="grids_1_of_3">
+				<div class="grid_1_of_3 images_1_of_3">
+					  <img src="images/icon1.jpg">
+                    <h3></h3>
+					  <p>Library is an combination of peace and love .<p>
+                    <p>Every person weather a child or of any age feel concentrated in library room.<p>
+            </div>
+    
+				<div class="grid_1_of_3 images_1_of_3">
+					  <img src="images/icon2.jpg">
+                    <h3></h3>
+					  <p>Library is the room where student ,teacher or any other community and religion,age can come and study and gain the knowledge.<p>
+                    <p>knowledge gained is then spread to some illeterate and like this one day our India will have a high literacy rate.</p>
+				</div>
+				<div class="grid_1_of_3 images_1_of_3">
+					  <img src="images/icon3.jpg">
+                    <h3></h3>
+					  <p>Llibrary is simply the repertoire of knowledgein a close room .<p>
+					  <P>Library teach us the time management , sincerity.   </P>
+				</div>
+				<div class="clear"></div>
+			</div>
+</div>
+</div>
+<!--main-->
+
+
+<!--footer-->
+<div class="footer-bg">	
+<div class="wrap">
+<div class="footer">
+  <div class="box1">
+	<h4 class="btm">What We Do</h4>
+	<p>we provide and serve the secured file and account to the user.</p>
+	<p>we provide the repertoire of kmowledge in the form of system and system into the capsule. Utilize and share the knowledge.</p>
+  </div>
+   <div class="box1">
+	<h4 class="btm">Categories</h4>
+	<nav>
+		<ul>
+			<li><a href="#">As per our LMS it is categorised into different types</a></li>
+			<li><a href="#">on the basis of features and on the basis of interaction interface. </a></li>
+			<li><a href="#"> Feature exist islibraraian login,student login,no of books available,amd many more which is showned in dashboard. </a></li>
+			<li><a href="#">The lms interact between student and librarian . </a></li>
+			<li><a href="#">It brings the reliability and flexibility to both the user. </a></li>
+			
+	    </ul>
+	</nav>
+	</div>
+	<div class="box1">
+		<h4 class="btm">Get in touch</h4>
+		<div class="box1_address">
+			<p>Bhadbhada Road,</p>
+			<p>Ratibad,Bhopal,</p>
+			<p>(M.P)</p>
+			<p>Phone:0755-2896303</p>
+			
+			
+				   		<p>Follow on: <a href="#">Facebook</a>, <a href="#">Twitter</a></p>
+
+		</div>
+	</div>
+	<div class="clear"></div>			
+</div>
+</div>
+</div>
+<!--footer1-->
+<div class="ftr-bg">
+	<div class="wrap">
+		<div class="footer">
+            <div class='h_bt'>
+		<div class='cssmenu'>
+			<ul>
+			    
+			    
+                <li><a href='./librarian/login.php'><span>Libraryan Login</span></a></li>
+            </ul>
+                </div>
+            </div>
+		<div class="copy">
+        
+			<p class="w3-link">© 2018 RGI-LIBRARY-SYSTEM. All Rights Reserved | Design by&nbsp; <a href="">Ankit Sah</a></p>
+		</div>
+		<div class="clear"></div>	
+	</div>
+	</div>
+</div>
+</body>
+</html>
